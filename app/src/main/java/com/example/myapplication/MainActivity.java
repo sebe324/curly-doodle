1892ab16af9b1d3569894147f27e32f9fc4fc752
sebe324 <sebeq05@gmail.com>
2023-04-24 09:29:32 +0200

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button buttonConfirm = (Button) findViewById(R.id.buttonConfirm);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
